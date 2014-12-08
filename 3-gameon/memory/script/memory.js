2461ad14a303f5df7119f5e26f08baacdbc2370b
Oliver Olsson <oo222bu@student.lnu.se>
2014-12-08 22:04:49 +0000

    done with memory

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -1,6 +1,8 @@
 "use strict";
 var Memory = {
     array: [],
+    tries:0,
+    counter:0,
     
     init:function(){
         var memoryArr;
@@ -8,7 +10,64 @@ var Memory = {
         var col = 4;
         
         memoryArr = RandomGenerator.getPictureArray(row, col);
-        console.log(memoryArr);
+       
+        Memory.setGame(memoryArr);
+    },
+    
+    setGame:function(memoryArr){
+            var divMem;
+            var aMem;
+            var imgMem;
+        for (var i = 0; i < memoryArr.length; i++) {
+            divMem = document.getElementById("memoryBoard");
+            aMem = document.createElement("a");
+            imgMem = document.createElement("img");
+            
+            aMem.setAttribute("href", "#");
+            aMem.setAttribute("class", memoryArr[i]);
+            imgMem.setAttribute("src", "pics/0.png");
+            
+            divMem.appendChild(aMem);
+            aMem.appendChild(imgMem);
+            
+            aMem.addEventListener("click", Memory.turnImg);
+        }
+    },
+    
+    turnImg:function(e){
+            e.preventDefault();
+            var click = this;
+            var number = click.getAttribute("class");
+            var counter;
+            if(Memory.array.length < 2){
+                click.firstChild.setAttribute("src", "pics/" + number +".png");
+                Memory.array.push(click);
+            }
+            
+            if(Memory.array.length >= 2){
+                if(Memory.array[0].getAttribute("class") === Memory.array[1].getAttribute("class"))
+                {
+                    Memory.countPair();
+                    Memory.array.splice(0, Memory.array.length);
+                }
+                else{
+                    setTimeout(function() 
+                        {
+                        for (var i = 0; i < Memory.array.length; i++) {
+                            Memory.array[i].firstChild.setAttribute("src", "pics/0.png");
+                        }
+                        Memory.array.splice(0, Memory.array.length);
+                    }, 1000);
+                }
+            }
+    },
+    
+    countPair:function(){
+       Memory.counter++;
+       console.log(Memory.counter);
+       if(Memory.counter === 8){
+           alert("Grattis du vann!");
+       }
     }
     
 };
