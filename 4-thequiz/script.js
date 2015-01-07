46f8d744b2625f714b4cf524134c213dc6e00de1
Oliver Olsson <oo222bu@student.lnu.se>
2015-01-07 17:57:00 +0000

    får tag i första frågan

--- 4-thequiz/script.js
+++ 4-thequiz/script.js
@@ -1,15 +1,52 @@
 "use strict";
 var Quizz = {
+    urlQuestion:"http://vhost3.lnu.se:20080/question/1",
+    question:[],
     
     init: function(){
       var start = document.getElementById("startbutton");
       
       start.addEventListener("click", function(e){
           start.setAttribute("value", "Skicka Svar!");
+          Quizz.buildElement();
+          Quizz.startQuizz();
       });
+      
+      
         
     },
-  
+    
+    buildElement:function(){
+        var question = document.getElementById("question");
+        var answer = document.getElementById("answer");
+        var textarea = document.getElementById("textarea");
+        var p = document.createElement("p");
+        var button = document.getElementById("startbutton");
+        
+        p.setAttribute("id", "p");
+        button.setAttribute("id", "answerbutton");
+        
+        question.appendChild(p);
+        
+    },
+    
+    startQuizz:function(){
+        var xhr = new XMLHttpRequest();
+        
+        xhr.onreadystatechange = function(){if(xhr.readyState === 4 && xhr.status === 200){Quizz.question=JSON.parse(xhr.responseText); console.log("Done"); Quizz.showQuestion(JSON.parse(xhr.responseText));}else{console.log("Läsfel, status:"+xhr.status);}};
+        
+        xhr.open("GET", Quizz.urlQuestion , true);
+        
+        xhr.send(null);
+    },
+    
+ 
+    
+    showQuestion:function(responseText){
+        var question = document.getElementById("p");
+        
+        question.innerHTML = responseText.question;
+    },
   
   
     
